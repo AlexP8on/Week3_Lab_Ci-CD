91a12bd1e707c717b1fa0f5b4407e417fe78beb2
AlexPaton <G00424185@atu.ie>
2024-10-02 14:10:29 +0100

    Started Project

---
+++ src/main/java/ie/atu/week3_lab_cicd/Week3LabCiCdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3_lab_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3LabCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3LabCiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week3_lab_cicd/Week3LabCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3_lab_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3LabCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
